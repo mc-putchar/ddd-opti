a2f42bcbd5832a825cf82c24e39b6a1693dcf80a
julesrb <jbjulesbernard3@gmail.com>
2024-10-15 14:38:44 +0200

    vite run config

---
+++ ddd-frontend/start-backend.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Compile the C++ server
+g++ main.cpp -o server -std=c++11 # TODO adapt to my drone ex
+
+# Run the server in the background and log its PID
+./server &
+SERVER_PID=$!
+
+# Capture the PID of the server for cleanup on exit
+echo "Server running with PID: $SERVER_PID"
+
+# Define a cleanup function to stop the server when the script stops
+cleanup() {
+    echo "Stopping C++ server..."
+    kill $SERVER_PID
+}
+trap cleanup EXIT
+
+# Keep the script alive to allow the server to run
+wait $SERVER_PID
