d8150056d631fdcdbe20a0b6652eecd24ebc10fd
julesrb <jbjulesbernard3@gmail.com>
2024-10-15 17:52:31 +0200

    vite run start config

--- ddd-app/src/App.jsx
+++ ddd-app/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls, Sphere, Box } from '@react-three/drei';
@@ -33,10 +33,30 @@ function App() {
 //       objectRef.current.rotation.set(...rotation);
 //     }
 //   });
+	const [message, setMessage] = useState('');
+	const [ws, setWs] = useState(null);
+
+	useEffect(() => {
+	const socket = new WebSocket('ws://0.0.0.0:18080/ws'); // Match the port of the C++ server
+	setWs(socket);
+
+	socket.onopen = () => {
+		console.log('Connected to WebSocket server');
+		socket.send('Hello from React');
+	};
+
+	socket.onmessage = (event) => {
+		console.log('Message from server:', event.data);
+		setMessage(event.data);
+	};
+
+	return () => socket.close();
+  }, []);
 
 	return (
 	  <Container fluid>
 		<h4>Stage Preview</h4>
+		<p>{message}</p>
 		<Col style={{ height: "800px", width: "1400px" }}>
 		  <Canvas
 			orthographic
@@ -48,22 +68,22 @@ function App() {
 			
 			 {/* Spotlight */}
 			 <spotLight 
-            position={[5, 5, 5]} // Positioning the light to the side
-            angle={0.3} // Spread of the light cone
-            penumbra={1} // Softness of the edges
-            intensity={1} // Brightness of the light
-            castShadow // Enable shadows
-          />
-          
-          {/* Cube (Light source) */}
-          {/* <Box position={[5, 5, 0]} args={[1, 1, 1]} >
-            <meshStandardMaterial attach="material" color="yellow" />
-          </Box> */}
-          
-          {/* Central Sphere */}
-          <Sphere position={[0, 0, 0]} args={[1, 32, 32]}>
-            <meshStandardMaterial attach="material" color="blue" />
-          </Sphere>
+			position={[5, 5, 5]} // Positioning the light to the side
+			angle={0.3} // Spread of the light cone
+			penumbra={1} // Softness of the edges
+			intensity={1} // Brightness of the light
+			castShadow // Enable shadows
+		  />
+		  
+		  {/* Cube (Light source) */}
+		  {/* <Box position={[5, 5, 0]} args={[1, 1, 1]} >
+			<meshStandardMaterial attach="material" color="yellow" />
+		  </Box> */}
+		  
+		  {/* Central Sphere */}
+		  <Sphere position={[0, 0, 0]} args={[1, 32, 32]}>
+			<meshStandardMaterial attach="material" color="blue" />
+		  </Sphere>
 			
 			{/* Controls and Helpers */}
 			<OrbitControls />
